80dc3010d1d58a698fe9c1d3c6ae8ed879fa0a2f
Andres Martinez <reymuerto14@hotmail.com>
2023-01-28 17:32:20 -0500

    instamos la dependencia de express

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+const port = 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello World')
@@ -9,4 +11,6 @@ app.get('*', (req, res) => {
     res.send('404 | Page not found')
   })
 
-app.listen(8080);
+app.listen(port, () => {
+  console.log(`Esta correindo en el pueerto${port}`)
+});
