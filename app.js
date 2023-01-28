1431a8363147c133a807702633da128d1e415e49
Andres Martinez <reymuerto14@hotmail.com>
2023-01-28 17:46:52 -0500

    se corrigio errores

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
+app.get('/hola-mundo', (req, res) => {
+  res.send('Hola mundo desde su respectiva ruta')
+})
+
 app.get('*', (req, res) => {
     res.send('404 | Page not found')
   })
 
 app.listen(port, () => {
-  console.log(`Esta correindo en el pueerto${port}`)
+  console.log(`Esta correindo en el puerto${port}`)
 });
