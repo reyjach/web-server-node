d59596cc72f8bdea26862951e5f506bf51a6df50
Andres Martinez <reymuerto14@hotmail.com>
2023-01-28 18:26:33 -0500

    se creo un carpeta publica con html y css

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ const app = express();
 
 const port = 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+//Servir contenido statico
+app.use( express.static('public'));
+
 
 app.get('/hola-mundo', (req, res) => {
-  res.send('Hola mundo desde su respectiva ruta')
+  res.send('Hola mundo desde su respectiva ruta');
 })
 
 app.get('*', (req, res) => {
-    res.send('404 | Page not found')
+    res.sendFile(__dirname + '/public/404.html');
   })
 
 app.listen(port, () => {
-  console.log(`Esta correindo en el puerto${port}`)
+  console.log(`Esta correindo en el puerto${port}`);
 });
